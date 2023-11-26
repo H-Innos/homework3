07d0fca3601366034bddfaa402f150e18691cfee
Henrik Innos <henrik.innos@gmail.com>
2023-11-26 19:32:01 +0200

    removed json server

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,112 @@
-import axios from 'axios';
+/*import axios from 'axios';*/
 import { createStore } from 'vuex';
 
 export default createStore({
   state: {
-    posts: [],
+    "posts": [
+      {
+          "id": 0,
+          "author_name": null,
+          "profile_picture": null,
+          "date": "2023-10-22",
+          "content_type": "text",
+          "text_content": "test post 1",
+          "image_content": null,
+          "likes": 0
+      },
+      {
+          "id": 1,
+          "author_name": null,
+          "profile_picture": null,
+          "date": "2023-10-22",
+          "content_type": "image",
+          "text_content": null,
+          "image_content": "/img/testpilt1.png",
+          "likes": 0
+      },
+      {
+          "id": 2,
+          "author_name": null,
+          "profile_picture": null,
+          "date": "2023-10-22",
+          "content_type": "text",
+          "text_content": "test post 2",
+          "image_content": null ,
+          "likes": 0
+      },
+      {
+          "id": 3,
+          "author_name": null,
+          "profile_picture": null,
+          "date": "2023-10-22",
+          "content_type": "mixed",
+          "text_content": "dog",
+          "image_content": "/img/testpilt2.jpg",
+          "likes": 0
+      },
+      {
+          "id": 4,
+          "author_name": null,
+          "profile_picture": null,
+          "date": "2023-06-15",
+          "content_type": "text",
+          "text_content": "longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext longtext",
+          "image_content": null,
+          "likes": 0
+      },
+      {
+          "id": 5,
+          "author_name": null,
+          "profile_picture": null,
+          "date": "2023-05-23",
+          "content_type": "mixed", 
+          "text_content": "mike",
+          "image_content": "/img/testpilt3.png",
+          "likes": 0
+      },
+      {
+          "id": 6,
+          "author_name": null,
+          "profile_picture": null,
+          "date": "2023-05-22",
+          "content_type": "text",
+          "text_content": "test post 3",
+          "image_content": null,
+          "likes": 0
+      },
+      {
+          "id": 7,
+          "author_name": null,
+          "profile_picture": null,
+          "date": "2023-04-13",
+          "content_type": "image",
+          "text_content": null,
+          "image_content": "/img/testpilt4.png",
+          "likes": 0
+      },
+      {
+          "id": 8,
+          "author_name": null,
+          "profile_picture": null,
+          "date": "2023-03-03",
+          "content_type": "text",
+          "text_content": "test post 4",
+          "image_content": null,
+          "likes": 0
+      },
+      {
+          "id": 9,
+          "author_name": null,
+          "profile_picture": null,
+          "date": "2023-01-01",
+          "content_type": "text",
+          "text_content": "test post 5",
+          "image_content": null,
+          "likes": 0
+      }
+  ]
   },
+  /* implementation with json server
   getters: {
     getPosts: state => state.posts,
   },
@@ -23,4 +125,5 @@ export default createStore({
       }
     },
   },
+  */
 });
